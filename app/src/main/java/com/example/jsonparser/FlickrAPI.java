752f6c37114058814cfc4a375f229a7a441cc382
Noureddine Achibane <nachibane27@gmail.com>
2022-02-01 14:13:39 +0100

    HH

--- app/src/main/java/com/example/jsonparser/FlickrAPI.java
+++ app/src/main/java/com/example/jsonparser/FlickrAPI.java
@@ -1,22 +1,6 @@
 package com.example.jsonparser;
 
-import android.os.AsyncTask;
 import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class FlickrAPI {
     MainActivity mainActivity;
--- app/src/main/java/com/example/jsonparser/InteringPics.java
+++
@@ -1,9 +0,0 @@
-package com.example.jsonparser;
-
-public class InteringPics {
-    String URL_Photos;
-
-    public InteringPics(String URL_Photos) {
-        this.URL_Photos = URL_Photos;
-    }
-}
